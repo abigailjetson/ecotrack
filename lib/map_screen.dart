479cfe9018b5a9186260a7c6b3d554fde154de1a
Abigail Jetson <abigailjetson@gmail.com>
2026-01-17 10:18:43 +0100

    Adding GPS

--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  double? lat;
+  double? lng;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  Future<void> getLocation() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high,
+    );
+
+    setState(() {
+      lat = position.latitude;
+      lng = position.longitude;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Nearby Recycling Points')),
       body: Center(
-        child: Container(
-          width: double.infinity,
-          height: 400,
-          color: Colors.grey[300],
-          child: Center(child: Text('Map Placeholder')),
-        ),
+        child: lat == null
+            ? CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Your Location:',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 16),
+                  Text('Latitude: $lat'),
+                  Text('Longitude: $lng'),
+                  SizedBox(height: 32),
+                  Container(
+                    width: 300,
+                    height: 300,
+                    color: Colors.grey[300],
+                    child: Center(child: Text('Map Placeholder')),
+                  ),
+                ],
+              ),
       ),
     );
   }
